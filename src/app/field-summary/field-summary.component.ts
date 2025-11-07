ed1599958f51d5e75b9dbb9d337b2059194e57cc
amboydan <amboydan@protonmail.com>
2025-11-06 17:11:25 -0900

    started on mat table for well list in field

--- src/app/field-summary/field-summary.component.ts
+++ src/app/field-summary/field-summary.component.ts
@@ -6,7 +6,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppStateService } from '../app-state.service';
 import { HakConnectionsService } from '../hak-connections.service';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms'; // Import FormBuilder, FormGroup
-
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import {
   trigger,
   state,
@@ -15,10 +17,41 @@ import {
   transition
 } from '@angular/animations'
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+export interface WellsData {
+  well: string;
+  type: string;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-field-summary',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatButtonToggleModule, MatIconModule, AsyncPipe, ReactiveFormsModule],
+  imports: [
+    CommonModule, MatToolbarModule, 
+    MatButtonToggleModule, MatIconModule, 
+    AsyncPipe, ReactiveFormsModule,
+    MatTableModule, MatCheckboxModule
+  ],
   templateUrl: './field-summary.component.html',
   styleUrls: ['./field-summary.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -35,6 +68,8 @@ import {
         ])
   ]
 })
+
+
 export class FieldSummaryComponent implements OnInit { // Implement OnInit
 
   filters = ['Week', 'Month', 'Year', 'All Time'];
@@ -62,7 +97,7 @@ export class FieldSummaryComponent implements OnInit { // Implement OnInit
     });
 
     this.loadFieldWells();
-    console.log(this.wells);
+    console.log(this.fieldWells);
   }
 
   selectField(field: string) {
@@ -70,7 +105,7 @@ export class FieldSummaryComponent implements OnInit { // Implement OnInit
   }
 
   selectedField: string | null = null;
-  wells: any[] = [];
+  fieldWells: any[] = [];
   loading: boolean = false;
   errorMessage: string | null = null;
   
@@ -82,7 +117,7 @@ export class FieldSummaryComponent implements OnInit { // Implement OnInit
 
     this.hakConnectionsService.getFieldWells(this.selectedField).subscribe({
       next: (data) => {
-        this.wells = data || [];
+        this.fieldWells = data || [];
         this.loading = false;
         console.log(data)
       },
@@ -92,5 +127,35 @@ export class FieldSummaryComponent implements OnInit { // Implement OnInit
         this.loading = false;
       }
     });
+  }
+
+  // this should probably be in its own module. 
+  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
 }
